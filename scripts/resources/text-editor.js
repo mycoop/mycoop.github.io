a9be9ec8c41bb33d43364e12db6673494b84d138
Kirill Gusev <mr.gusev.k@gmail.com>
2014-07-28 19:17:47 +0400

    Fixed editor config url.

--- scripts/resources/text-editor.js
+++ scripts/resources/text-editor.js
@@ -2,8 +2,8 @@ angular.module('resources.textEditor',[])
     .factory('TextEditor', ['$http',function($http){
         var service = {
            getConfig: function(fileId, callback){
-//               $http.get('/api/editor?'+fileId).success(callback);
-               callback({validateKey: '12321321'})
+               $http.get('/api/editor?fileId='+fileId).success(callback);
+             //  callback({validateKey: '12321321'})
            }
         };
         return service;
